5cd77c066b2c3de52cd692bab633c584c2a1184b
LGGC <liran12369@gmail.com>
2022-09-08 21:28:48 +0300

    add menu functionality

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,21 @@
 const menuLogic = (() => {
-  const _menu = document.getElementById("menu");
+  const _menuSlots = document.getElementById("menu").children;
+
+  const _toggleSelect = (slotTarget) => {
+    Array.from(_menuSlots).forEach((slot) => {
+      slot.classList.remove("selected");
+    });
+
+    slotTarget.classList.add("selected");
+  };
+
+  const _setEvents = () => {
+    Array.from(_menuSlots).forEach((slot) => {
+      slot.addEventListener("click", (e) => _toggleSelect(e.currentTarget));
+    });
+  };
+
+  _setEvents();
 })();
 
 export { menuLogic };
